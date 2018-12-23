f21a63240f9bbdf1406cbb9cbf7a24ad1a1a5548
Kleckov Gleb <d91580433832@yandex.ru>
2018-12-24 02:26:18 +0300

    main,js

--- Geom/main.js
+++ Geom/main.js
@@ -97,7 +97,7 @@ input.addEventListener('input', async ()=>{
 
     data.forEach(elem=>{
         const answer = document.createElement('div')
-        answer.innerHTML = `<h4>${elem.elem}</h4> <a href="${link(elem.j)}">Ссылка</a>`
+        answer.innerHTML = `<h4>${elem.elem}</h4> <a href="${link(elem.j)} target="__blank">Ссылка</a>`
         document.querySelector('.answer').appendChild(answer)
     })
 
