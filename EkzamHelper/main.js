4232e9a72414dc6aae54087de6fd3dfda38172ec
Gleb Kletskov <gfgfddgleb@gmail.com>
2019-10-23 09:46:43 +0300

    Add all questions for philospohy

--- EkzamHelper/main.js
+++ EkzamHelper/main.js
@@ -2,23 +2,40 @@ window.onload = () => {
   const questionNodes = document.querySelectorAll('.question');
   
   const questions = [...questionNodes].map((questionElem) => {
-    const result = {
-      question: questionElem.querySelector('.question-text').innerHTML,
-      answer: questionElem.querySelector('.question-answer').innerHTML,
+    let question;
+    let answer;
+
+
+    try {
+      question = questionElem.querySelector('.question-text').innerHTML.trim().toLowerCase();
+      answer = questionElem.querySelector('.question-answer').innerHTML.trim().toLowerCase();
+    } catch {
+      question = '';
+      answer = '';
     }
-    return  result;
+
+    return  {
+      question,
+      answer
+    };
   });
 
   const input = document.querySelector('.question-input');
   const answer = document.querySelector('.answer');
   
   input.addEventListener('input', (evt) => {
-    const userInput = evt.target.value.trim();
+    const userInput = evt.target.value.trim().toLowerCase();
     answer.innerHTML = '';
     questions.forEach((question) => {
+      let countOfAnswers = 0;
       if (question.question.startsWith(userInput)) {
-        const answerString = ` ***** \n ${question.answer} \n  ***** <br> `;
+        countOfAnswers++;
+        const answerString = ` ***** \n ${question.answer} \n  ***** <br> <br>`;
         answer.innerHTML += answerString;
+      }
+
+      if (countOfAnswers > 3) {
+        answer.innerHTML = '';
       }
     });
 
