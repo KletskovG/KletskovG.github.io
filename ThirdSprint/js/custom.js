4dbbb0ffe7db19ee1a36babb22c8adf5328e2b80
Gleb Kletskov <gfgfddgleb@gmail.com>
2021-07-18 21:47:48 +0300

    update for sprints

---
+++ ThirdSprint/js/custom.js
@@ -0,0 +1,69 @@
+"use strict";
+$(document).ready(function() {
+
+    $("select").niceSelect();
+
+    /*>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+        AOS Animation Activation
+    <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<*/
+    AOS.init();
+    window.addEventListener("load", AOS.refresh);
+    AOS.init({
+        once: true
+    })
+
+    /*>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>      
+          Counter Up Activation
+    <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<*/
+    $('.counter').counterUp({
+        delay: 10,
+        time: 2000
+    });
+
+    /*>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>      
+           Sticky Header
+    <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<*/
+    window.onscroll = function() {
+        scrollFunction();
+    };
+
+    function scrollFunction() {
+        if (
+            document.body.scrollTop > 50 ||
+            document.documentElement.scrollTop > 50
+        ) {
+            $(".site-header--sticky").addClass("scrolling");
+        } else {
+            $(".site-header--sticky").removeClass("scrolling");
+        }
+        if (
+            document.body.scrollTop > 700 ||
+            document.documentElement.scrollTop > 700
+        ) {
+            $(".site-header--sticky.scrolling").addClass("reveal-header");
+        } else {
+            $(".site-header--sticky.scrolling").removeClass("reveal-header");
+        }
+    }
+
+    /*>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>      
+           Smooth Scroll
+    <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<*/
+
+    $(".goto").on("click", function(event) {
+        if (this.hash !== "") {
+            event.preventDefault();
+            var hash = this.hash;
+            $("html, body").animate({
+                    scrollTop: $(hash).offset().top,
+                },
+                2000,
+                function() {
+                    window.location.hash = hash;
+                }
+            );
+        } // End if
+    });
+
+});
+
